381d246b4ecaee266b558ea75ff0baec062dce93
Daniel Caban <DCaban91@gmail.com>
2023-06-29 14:20:05 -0400

    Added styling for happy women image on catalog page

--- ClientApp/src/components/SummaryContainer.js
+++ ClientApp/src/components/SummaryContainer.js
@@ -12,7 +12,7 @@ const SummaryContainer = ({ title, description, imageUrl, altText }) => {
             {
                 // Only render when both imageUrl and altText is passed in
                 imageUrl && altText ? (
-                <div className="summary-container__people-drinking-image d-none d-md-block">
+                    <div className={styles["summary-container__people-drinking-image"] + " d-none d-md-block"}>
                     <img src={imageUrl} alt={altText} />
                 </div>
             ) : null}
